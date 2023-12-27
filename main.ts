77dea2d6b11f115a18b6ba454e68bde0a0d123d7
Ian Schoen <ian.rocha.schoen@gmail.com>
2023-12-27 16:12:45 -0700

    Add routes; validation; more opinionated routing

--- main.ts
+++ main.ts
@@ -1,49 +1,108 @@
 import { compact } from "./utils/compact.ts";
 import { flattenList } from "./utils/flattenList.ts";
-import { Context, Handler, Hono, isAbsolute } from "./deps.ts";
+import {
+  Context,
+  Handler,
+  Hono,
+  isAbsolute,
+  MiddlewareHandler,
+  validator,
+} from "./deps.ts";
 
 type RoutingConfig = {
   path: string;
-  filePattern: string | RegExp;
-  fnPattern: string;
+  filePattern?: string | RegExp;
 };
 
 type RouteResult = {
   path: string;
-  fn: Handler;
+  GET?: { validators: MiddlewareHandler[]; handler: Handler };
+  PUT?: { validators: MiddlewareHandler[]; handler: Handler };
+  POST?: { validators: MiddlewareHandler[]; handler: Handler };
+  DELETE?: { validators: MiddlewareHandler[]; handler: Handler };
 };
 
 const getRoutePaths = async (
   routeConfig: RoutingConfig,
 ): Promise<RouteResult[]> => {
-  const { path, filePattern, fnPattern } = routeConfig;
+  const { path, filePattern } = routeConfig;
 
   const routes = Array.from(Deno.readDirSync(path));
+
+  if (
+    routes.some((r) => r.name === "page.tsx" || r.name === "page.ts") &&
+    routes.some((r) => r.name === "route.ts" || r.name === "route.tsx")
+  ) {
+    throw new Error(
+      `Route ${path} has both page.ts(x) and route.ts(x) files. Please remove one.`,
+    );
+  }
 
   const parsedRoutes = await Promise.all(routes.map(async (r) => {
     if (r.isDirectory) {
       const nestedRoutes = getRoutePaths({
         path: `${path}/${r.name}`,
         filePattern,
-        fnPattern,
       });
 
       return nestedRoutes;
     }
 
-    if (
-      (filePattern instanceof RegExp && filePattern.test(r.name)) ||
-      r.name === filePattern
-    ) {
-      if (!isAbsolute(path)) {
-        throw new Error(`Path ${path} is not absolute`);
-      }
+    const file = await import(`file://${path}/${r.name}`);
 
-      const fn = (await import(`file://${path}/${r.name}`))[fnPattern];
+    if (r.name === "page.ts" || r.name === "page.tsx") {
+      const PAGE = file["default"];
+      const PAGE_VALIDATORS = file["PAGE_VALIDATORS"] ?? [];
+
+      console.log({ PAGE });
 
       return [{
         path,
-        fn: (ctx: Context) => ctx.html(fn(ctx)),
+        GET: PAGE
+          ? {
+            validators: PAGE_VALIDATORS,
+            handler: (ctx: Context) => PAGE(ctx),
+          }
+          : undefined,
+      }];
+    }
+
+    if (r.name === "route.ts" || r.name === "route.tsx") {
+      const GET = file["GET"];
+      const GET_VALIDATORS = file["GET_VALIDATORS"] ?? [];
+      const PUT = file["PUT"];
+      const PUT_VALIDATORS = file["PUT_VALIDATORS"] ?? [];
+      const POST = file["POST"];
+      const POST_VALIDATORS = file["POST_VALIDATORS"] ?? [];
+      const DELETE = file["DELETE"];
+      const DELETE_VALIDATORS = file["DELETE_VALIDATORS"] ?? [];
+
+      return [{
+        path,
+        GET: GET
+          ? {
+            validators: GET_VALIDATORS,
+            handler: (ctx: Context) => GET(ctx),
+          }
+          : undefined,
+        PUT: PUT
+          ? {
+            validators: PUT_VALIDATORS,
+            handler: (ctx: Context) => PUT(ctx),
+          }
+          : undefined,
+        POST: POST
+          ? {
+            validators: POST_VALIDATORS,
+            handler: (ctx: Context) => POST(ctx),
+          }
+          : undefined,
+        DELETE: DELETE
+          ? {
+            validators: DELETE_VALIDATORS,
+            handler: (ctx: Context) => DELETE(ctx),
+          }
+          : undefined,
       }];
     }
   }));
@@ -56,6 +115,10 @@ export const getRoutes = async (
 ) => {
   const { path: rootPath } = routeConfig;
 
+  if (!isAbsolute(rootPath)) {
+    throw new Error(`Path ${rootPath} is not absolute`);
+  }
+
   const routes = await getRoutePaths(routeConfig);
 
   const finalRoutes = routes.map((r) =>
@@ -67,7 +130,12 @@ export const getRoutes = async (
   const app = new Hono();
 
   for (const finalRoute of finalRoutes) {
-    app.get(finalRoute.path, finalRoute.fn);
+    const { path, GET, PUT, POST, DELETE } = finalRoute;
+
+    GET && app.get(path, ...GET.validators, GET.handler);
+    PUT && app.put(path, ...PUT.validators, PUT.handler);
+    POST && app.post(path, ...POST.validators, POST.handler);
+    DELETE && app.delete(path, ...DELETE.validators, DELETE.handler);
   }
 
   return app;
