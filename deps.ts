6f587127adda6011c4e510633e3323678e246f06
iSchoen <ian.rocha.schoen@gmail.com>
2024-09-21 01:49:54 -0600

    Delete deps.ts

--- deps.ts
+++
@@ -1,7 +0,0 @@
-export { Hono } from "https://deno.land/x/hono@v3.11.9/mod.ts";
-export type {
-  Context,
-  Handler,
-  MiddlewareHandler,
-} from "https://deno.land/x/hono@v3.11.9/mod.ts";
-export { isAbsolute } from "https://deno.land/std@v0.210.0/path/mod.ts";
